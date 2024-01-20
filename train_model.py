21cadac852ad4af13ca69b4d53d7800a27facdf3
Eddwin Cheteni <eddychetz@gmail.com>
2024-01-20 16:04:10 +0200

    update code

--- train_model.py
+++ train_model.py
@@ -1,1 +1,12 @@
-import pytimetk as tk
+import pandas as pd
+
+print(f">>> Importing data.....")
+data = pd.read_csv("./data/GOOGL_stock_price_data.csv")
+print(data.head())
+print()
+print(f">>> Loading Completed.")
+print()
+print(f">>> Inspecting data.....")
+print(data.info())
+print(f">>> The data has {data.shape[0]} rows and {data.shape[1]} columns.")
+print()
