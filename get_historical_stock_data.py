2c7520b769374a39db8bddd80aa0a0f22fcea51f
Eddwin Cheteni <eddychetz@gmail.com>
2024-01-20 15:29:51 +0200

    update code

--- get_historical_stock_data.py
+++ get_historical_stock_data.py
@@ -19,13 +19,21 @@ import yfinance as yf
 
 def get_historical_stock_data(tickers: str, period: str, interval: str) -> pd.DataFrame:
 
-  # Acess yf API
+  # Access yf API
+  print(f'>>> Downloading {tickers} Stock Data from yfinance Website.')
   data = yf.download(tickers = tickers, period = period, interval = interval)
+  
+  print(f'>>> Downloading {tickers} is Complete.')
 
   # Write the merged dataframe to a CSV file
-  save_path = "data/{}_stock_prices.csv".format(tickers)
-    
-  print('Save at :',save_path)
+  save_path = "data/{}_stock_price_data.csv".format(tickers)
+
   pd.DataFrame(data).to_csv(save_path, index=False)
-      
+  print(f'>>> Saving {tickers} Stock Data.')  
+  print(f'>>> Saving {tickers} is Complete and Saved at: {save_path}')
   return data
+
+tickers = ["GOOGL", "AAPL", "MSFT", "TSLA"]
+
+for symbol in tickers:
+  get_historical_stock_data(tickers=symbol, period="12mo", interval="1h")
--- train_model.py
+++ train_model.py
@@ -1,1 +1,1 @@
-
+import pytimetk as tk
